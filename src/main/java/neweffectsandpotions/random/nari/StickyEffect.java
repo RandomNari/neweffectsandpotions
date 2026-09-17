a64b37dc20d1b747df6bfd69dce51e2c9dd913fd
RandomNari <giotareddragon@gmail.com>
2026-09-17 17:17:43 +0200

    it took pretty long but now you can climb on blocks with a livingentity mixin, wasnt able to do it with the effect class itself, the problem now is that you can also climb on air T-T ill need to fix that haha

--- src/main/java/neweffectsandpotions/random/nari/StickyEffect.java
+++ src/main/java/neweffectsandpotions/random/nari/StickyEffect.java
@@ -1,12 +1,23 @@
 package neweffectsandpotions.random.nari;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 
 public class StickyEffect extends MobEffect {
     protected StickyEffect()
     {
         super(MobEffectCategory.BENEFICIAL,0x1);
+
     }
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+        return true;
+    }
+
 }
---
+++ src/main/java/neweffectsandpotions/random/nari/mixin/LivingEntityMixin.java
@@ -0,0 +1,27 @@
+package neweffectsandpotions.random.nari.mixin;
+
+import net.minecraft.world.entity.InterpolationHandler;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import neweffectsandpotions.random.nari.ModEffects;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+
+    @Inject(at = @At("HEAD"), method = "onClimbable", cancellable = true)
+	public void init(CallbackInfoReturnable<Boolean> cir) {
+        if((Object)this instanceof Player player){
+
+		if(player.hasEffect(ModEffects.STICKY))
+        {
+        cir.setReturnValue(true);
+        }
+        }
+	}
+}
