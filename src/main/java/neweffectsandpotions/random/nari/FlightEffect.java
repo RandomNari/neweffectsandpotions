818cf632d39a8f7d7ad0e7f44d56bfbe9fe89746
RandomNari <giotareddragon@gmail.com>
2026-09-17 14:09:23 +0200

    managed to make the flight effect work after a few problems T-T, but it does work now so that's great

--- src/main/java/neweffectsandpotions/random/nari/FlightEffect.java
+++ src/main/java/neweffectsandpotions/random/nari/FlightEffect.java
@@ -1,11 +1,43 @@
 package neweffectsandpotions.random.nari;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import org.apache.logging.log4j.core.jmx.Server;
 
 public class FlightEffect extends MobEffect {
     protected FlightEffect ()
     {
         super(MobEffectCategory.BENEFICIAL,0x1);
+    }
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+        return true;
+    }
+    @Override
+    public boolean applyEffectTick(ServerLevel level, LivingEntity entity, int amplifier){
+        if (entity instanceof ServerPlayer player) {
+            player.getAbilities().mayfly = true;
+            player.onUpdateAbilities();
+        }
+        return true;
+    }
+    @Override
+    public void onEffectRemoved(MobEffectInstance mobEffectInstance, LivingEntity entity){
+        if(entity instanceof ServerPlayer player)
+        {if(!player.isCreative()&&!player.isSpectator()){
+        player.getAbilities().mayfly = false;
+        player.getAbilities().flying = false;
+        player.onUpdateAbilities();
+        }}
+
+
+        super.onEffectRemoved(mobEffectInstance, entity);
     }
 }
