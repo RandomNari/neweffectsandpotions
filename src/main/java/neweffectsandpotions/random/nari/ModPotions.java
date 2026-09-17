8027159122de753a28702a569ed61e58c7da7c16
RandomNari <giotareddragon@gmail.com>
2026-09-18 01:10:15 +0200

    I now fixed errors i had and added longer potions that are brewable. i also made sprites (for sticky/bouncy I just used the standard sting/slimeball texture). Also made a lang file so that the potions/effects are named properly.

--- src/main/java/neweffectsandpotions/random/nari/ModPotions.java
+++ src/main/java/neweffectsandpotions/random/nari/ModPotions.java
@@ -8,26 +8,26 @@ import net.minecraft.world.item.alchemy.Potion;
 
 public class ModPotions {
     public static final Holder<Potion> FLIGHT_POTION = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.FLIGHT_POTION, new Potion("flight", new MobEffectInstance(ModEffects.FLIGHT, 3600, 0)));
-    public static final Holder<Potion> FLIGHT_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.FLIGHT_POTION, new Potion("flight", new MobEffectInstance(ModEffects.FLIGHT, 8400, 0)));
+    public static final Holder<Potion> FLIGHT_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.FLIGHT_POTION_LONG, new Potion("flight_long", new MobEffectInstance(ModEffects.FLIGHT, 8400, 0)));
 
     public static final Holder<Potion> CURSE_OF_THE_ZOMBIE_POTION = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.CURSE_OF_THE_ZOMBIE_POTION, new Potion("curse_of_the_zombie", new MobEffectInstance(ModEffects.COTZ, 3600, 0)));
-    public static final Holder<Potion> CURSE_OF_THE_ZOMBIE_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.CURSE_OF_THE_ZOMBIE_POTION, new Potion("curse_of_the_zombie", new MobEffectInstance(ModEffects.COTZ, 8400, 0)));
+    public static final Holder<Potion> CURSE_OF_THE_ZOMBIE_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.CURSE_OF_THE_ZOMBIE_POTION_LONG, new Potion("curse_of_the_zombie_long", new MobEffectInstance(ModEffects.COTZ, 8400, 0)));
 
     public static final Holder<Potion> STICKY_POTION = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.STICKY_POTION, new Potion("sticky", new MobEffectInstance(ModEffects.STICKY, 3600, 0)));
-    public static final Holder<Potion> STICKY_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.STICKY_POTION, new Potion("sticky", new MobEffectInstance(ModEffects.STICKY, 8400, 0)));
+    public static final Holder<Potion> STICKY_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.STICKY_POTION_LONG, new Potion("sticky_long", new MobEffectInstance(ModEffects.STICKY, 8400, 0)));
 
     public static final Holder<Potion> SHRINK_POTION_0 = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.SHRINK_POTION, new Potion("shrink", new MobEffectInstance(ModEffects.SHRINK, 3600, 0)));
-    public static final Holder<Potion> SHRINK_POTION_1 = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.SHRINK_POTION, new Potion("shrink", new MobEffectInstance(ModEffects.SHRINK, 3600, 1)));
-    public static final Holder<Potion> SHRINK_POTION_0_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.SHRINK_POTION, new Potion("shrink", new MobEffectInstance(ModEffects.SHRINK, 8400, 0)));
-    public static final Holder<Potion> SHRINK_POTION_1_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.SHRINK_POTION, new Potion("shrink", new MobEffectInstance(ModEffects.SHRINK, 8400, 1)));
+    public static final Holder<Potion> SHRINK_POTION_1 = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.SHRINK_POTION_1, new Potion("shrink_1", new MobEffectInstance(ModEffects.SHRINK, 3600, 1)));
+    public static final Holder<Potion> SHRINK_POTION_0_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.SHRINK_POTION_0_LONG, new Potion("shrink_0_long", new MobEffectInstance(ModEffects.SHRINK, 8400, 0)));
+    public static final Holder<Potion> SHRINK_POTION_1_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.SHRINK_POTION_1_LONG, new Potion("shrink_1_long", new MobEffectInstance(ModEffects.SHRINK, 8400, 1)));
 
     public static final Holder<Potion> GROWTH_POTION_0 = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.GROWTH_POTION, new Potion("growth", new MobEffectInstance(ModEffects.GROWTH, 3600, 0)));
-    public static final Holder<Potion> GROWTH_POTION_1 = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.GROWTH_POTION, new Potion("growth", new MobEffectInstance(ModEffects.GROWTH, 3600, 1)));
-    public static final Holder<Potion> GROWTH_POTION_0_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.GROWTH_POTION, new Potion("growth", new MobEffectInstance(ModEffects.GROWTH, 8400, 0)));
-    public static final Holder<Potion> GROWTH_POTION_1_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.GROWTH_POTION, new Potion("growth", new MobEffectInstance(ModEffects.GROWTH, 8400, 1)));
+    public static final Holder<Potion> GROWTH_POTION_1 = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.GROWTH_POTION_1, new Potion("growth_1", new MobEffectInstance(ModEffects.GROWTH, 3600, 1)));
+    public static final Holder<Potion> GROWTH_POTION_0_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.GROWTH_POTION_0_LONG, new Potion("growth_0_long", new MobEffectInstance(ModEffects.GROWTH, 8400, 0)));
+    public static final Holder<Potion> GROWTH_POTION_1_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.GROWTH_POTION_1_LONG, new Potion("growth_1_long", new MobEffectInstance(ModEffects.GROWTH, 8400, 1)));
 
     public static final Holder<Potion> BOUNCE_POTION = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.BOUNCE_POTION, new Potion("bounce", new MobEffectInstance(ModEffects.BOUNCE, 3600, 0)));
-    public static final Holder<Potion> BOUNCE_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.BOUNCE_POTION, new Potion("bounce", new MobEffectInstance(ModEffects.BOUNCE, 8400, 0)));
+    public static final Holder<Potion> BOUNCE_POTION_LONG = Registry.registerForHolder(BuiltInRegistries.POTION, ModPotionsIds.BOUNCE_POTION_LONG, new Potion("bounce_long", new MobEffectInstance(ModEffects.BOUNCE, 8400, 0)));
 
 
     public static void initialize() {
--- src/main/java/neweffectsandpotions/random/nari/ModPotionsIds.java
+++ src/main/java/neweffectsandpotions/random/nari/ModPotionsIds.java
@@ -22,6 +22,16 @@ public class ModPotionsIds {
     public static final ResourceKey<Potion> SHRINK_POTION = create("shrink");
     public static final ResourceKey<Potion> GROWTH_POTION = create("growth");
     public static final ResourceKey<Potion> BOUNCE_POTION = create("bounce");
+    public static final ResourceKey<Potion> FLIGHT_POTION_LONG = create("flight_long");
+    public static final ResourceKey<Potion> STICKY_POTION_LONG = create("sticky_long");
+    public static final ResourceKey<Potion> CURSE_OF_THE_ZOMBIE_POTION_LONG = create("curse_of_the_zombie_long");
+    public static final ResourceKey<Potion> SHRINK_POTION_0_LONG = create("shrink_0_long");
+    public static final ResourceKey<Potion> GROWTH_POTION_0_LONG = create("growth_0_long");
+    public static final ResourceKey<Potion> BOUNCE_POTION_LONG = create("bounce_long");
+    public static final ResourceKey<Potion> SHRINK_POTION_1_LONG = create("shrink_1_long");
+    public static final ResourceKey<Potion> GROWTH_POTION_1_LONG = create("growth_1_long");
+    public static final ResourceKey<Potion> SHRINK_POTION_1 = create("shrink_1");
+    public static final ResourceKey<Potion> GROWTH_POTION_1 = create("growth_1");
 
     private static ResourceKey<Potion> create(String name) {
         Identifier id = Identifier.fromNamespaceAndPath(String.valueOf(Neweffectsandpotions.MOD_ID), name);
